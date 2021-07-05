95bb366ee7c92801f2be6f39e2089c91c483ce47
Diego Rodriguez <roca230313@gmail.com>
2021-07-05 10:09:41 -0500

    no message

---
+++ C++test/cmake-build-debug/CMakeFiles/C__test.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/C__test.dir/main.cpp.obj"
+  "C__test.exe"
+  "C__test.exe.manifest"
+  "C__test.lib"
+  "C__test.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/C__test.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
